d8ecb1a640b80d4015bc7969a28adb23f9a17466
andrefetch <andresilvapersonal2007@gmail.com>
2026-04-22 11:31:50 -0400

    Redone most of the UI on the homepage, thanks bootstrap.

--- backend/main.py
+++ backend/main.py
@@ -10,15 +10,24 @@ app = Flask(
 @app.route("/")
 @app.route('/home')
 def home():
-    return render_template('home.jinja')
+    return render_template('home.jinja', title="Home")
 
 @app.route("/about")
 def about():
-    return render_template('about.jinja')
+    return render_template('about.jinja', title="About")
 
 @app.route("/chat")
 def chat():
-    return render_template('chat.jinja')
+    return render_template('chat.jinja', title="Chat")
+
+
+@app.route("/login")
+def login():
+    return render_template('auth/login.jinja', title="Login")
+
+@app.route('/signup')
+def signup():
+    return render_template('auth/signUp.jinja', title="Sign Up")
 
 if __name__ == "__main__":
     app.run(debug=True)
